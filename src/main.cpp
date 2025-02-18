45e588e56325ee00b1b91d2b07892c2aff925720
Morze <kronto70@gmail.com>
2025-02-18 20:02:04 +0300

    fx: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,7 @@ int main() {
     int x = 10, y = 4;
     std::cout << "Сложение: " << add(x, y) << std::endl;
     std::cout << "Вычитание: " << subtract(x, y) << std::endl;
+
+    std::cout << "Changed message in main branch!" << std::endl;
     return 0;
 }
