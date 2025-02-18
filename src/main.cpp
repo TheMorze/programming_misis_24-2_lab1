77dbfd9f4bc513158dd7db7634aab3fbd6ed6d73
Morze <kronto70@gmail.com>
2025-02-18 20:01:25 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,8 @@ int main() {
     int x = 10, y = 4;
     std::cout << "Сложение: " << add(x, y) << std::endl;
     std::cout << "Вычитание: " << subtract(x, y) << std::endl;
+
+    // Используем функцию умножения
+    std::cout << "Умножние: " << multiply(x, y) << std::endl;
     return 0;
 }
