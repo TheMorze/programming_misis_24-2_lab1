f7feaf2ccb0877eb8fd90fb23c83524939be346e
Morze <kronto70@gmail.com>
2025-02-18 19:42:49 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main() {
+    int x = 10, y = 4;
+    std::cout << "Сложение: " << add(x, y) << std::endl;
+    std::cout << "Вычитание: " << subtract(x, y) << std::endl;
+    return 0;
+}
